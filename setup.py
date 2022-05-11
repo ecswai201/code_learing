473cd3ff300cee0fd70c2d68938df01c2b1f29cc
ecswai201 <66985082+ecswai201@users.noreply.github.com>
2022-05-12 00:55:51 +0800

    修改代码行字符限制为79

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
-#! /usr/bin/env python
-# coding=utf-8
+"""
+-------------------------------------------------------------------------------
+Revision:    1.0
+Date:        2022-05-12
+Author:      ying.xue
+Description:
+预置条件：
+测试步骤：
+预期结果
+Modifier：
+Modifytime：
+Description of Modify：
+Email:      ying.xue@unisoc.com
+-------------------------------------------------------------------------------
+"""
+
+import platform
 
 from setuptools import setup, find_packages
-import platform
-requires =["pyserial>=3.4","uiautomator2","loguru"]
+
+requires = ["pyserial>=3.4", "uiautomator2", "loguru"]
 if platform.system().lower() == 'windows':
     requires.append("pywin32>=224")
     requires.append("pywin32-ctypes>=0.2.0")
